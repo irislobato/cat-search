080709973a02c75bf50b09a6bb8eeb3559058f2f
irislobato <irisblobato@gmail.com>
2026-09-09 15:11:00 -0300

    tooltip e css

--- src/CatCard.jsx
+++ src/CatCard.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import './CatCard.css'
+import Tooltip from "./Tooltip";
 
 export default function CatCard({ detalhes }) {
   return (
     <div className="card-gato">
       <h3 className="nome-raca">{detalhes.name}</h3>
       <div className="origem">
-        <h5 className="titulo-origem">Origem:</h5> <h5 className="lugar-origem">{detalhes.origin}</h5>
+        <h5 className="titulo-origem">Origem:</h5>{" "}
+        <h5 className="lugar-origem">{detalhes.origin}</h5>
       </div>
 
-      {detalhes.image_link && <img className="img-gato" src={detalhes.image_link} alt="" />}
-      <button className="ver-mais">Ver mais...</button>
+      {detalhes.image_link && (
+        <img className="img-gato" src={detalhes.image_link} alt="" />
+      )}
+
+      <Tooltip texto="Clique para saber mais detalhes">
+        <button className="ver-mais">Ver mais...</button>
+      </Tooltip>
     </div>
   );
 }
